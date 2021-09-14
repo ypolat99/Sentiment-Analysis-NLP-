ce92995e6ce47cffd0f214803fc03432ca41a7d6
ypolat99 <77054305+ypolat99@users.noreply.github.com>
2021-09-13 21:06:44 -0700

    Update sentiment.py
    Added Code for imports and data edits

--- sentiment.py
+++ sentiment.py
@@ -9,3 +9,21 @@ from sklearn import decomposition, ensemble
 import pandas, xgboost, numpy, textblob, string
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
+
+import pandas as pd
+
+# Get The Data
+data = pd.read_csv("train.tsv", sep="\t")
+#data.head()
+
+# Edit The Data
+data["Sentiment"].replace(0, value="negatif", inplace=True)
+data["Sentiment"].replace(1, value="negatif", inplace=True)
+data["Sentiment"].replace(3, value="pozitif", inplace=True)
+data["Sentiment"].replace(4, value="pozitif", inplace=True)
+#data.head()
+data = data[(data.Sentiment == "negatif") | (data.Sentiment == "pozitif")]
+#data.head()
+
+# Group The Data
+data.groupby("Sentiment").count()
