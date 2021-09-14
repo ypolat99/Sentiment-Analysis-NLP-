d56454a34cc0dad4b70f9fb4804b230e2e0c3980
ypolat99 <77054305+ypolat99@users.noreply.github.com>
2021-09-13 21:02:28 -0700

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,11 @@
 # import the libraries
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+#!pip install keras
+#!pip install tensorflow
+from textblob import TextBlob
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
